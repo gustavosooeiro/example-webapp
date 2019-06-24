57d9a1654e3cb176e0f15842f64d38a6515e9331
Gustavo Soeiro Senise <gustavosooeiro@gmail.com>
2019-06-24 14:39:47 -0300

    change Cluster to development-cluster

--- run-stack.sh
+++ run-stack.sh
@@ -8,5 +8,5 @@ aws cloudformation $ACTION \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=119541632154.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-ae36a9d4 \
-    ParameterKey=Cluster,ParameterValue=default \
+    ParameterKey=Cluster,ParameterValue=development-cluster \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:119541632154:listener/app/production-website/70aed30cb273b58b/63c43efa49905b7f
