39773a17bedb5bd5dbdb0bf472fad88699adf1c1
Gustavo Soeiro Senise <gustavosooeiro@gmail.com>
2019-06-25 08:50:20 -0300

    pass ALB to deploy job

--- run-stack.sh
+++ run-stack.sh
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=119541632154.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-ae36a9d4 \
     ParameterKey=Cluster,ParameterValue=homologacao \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:119541632154:listener/app/production-website/70aed30cb273b58b/63c43efa49905b7f
+    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
